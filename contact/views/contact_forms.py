24fc29c8555da107aca8c5f784091e6f8f35e79c
RaulTeles <telesraul@gmail.com>
2023-10-26 00:34:49 -0300

    Criando o formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -11,15 +11,55 @@ from django.http import Http404
 from django.db.models import Q
 #importando a função Paginator para criar uma paginação no site
 from django.core.paginator import Paginator
+#importando a biblioca form para utilização das classes
+from django import forms
+from django.core.exceptions import ValidationError
 
 
 ##################################################################################################
 
+#Criando uma classe para os formulários
+#passando o parametro ModelForm, pq o formulário vai ser criado em base aos models já existentes
+
+class contactForms(forms.ModelForm):
+    class Meta:
+        #definindo qual é model em que o formulário será criado
+        model = contact
+        #definindo quais campos serão exibidos nos forms
+        fields = ('first_name','last_name','phone')
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de error',
+                code='invalid'
+            )
+        )
+
+
 
 def create(request):
 
+    if request.method == 'POST':
+        context = {
+        #passando o formulário no contexto para poder ser mostrado no html
+        'form' : contactForms(request.POST),
+        }
+
+
+        return render(request,
+            'contact/create.html',
+            context,
+            )
+
+
 
     context = {
+        #passando o formulário no contexto para poder ser mostrado no html
+        'form' : contactForms(),
 
     }
 
